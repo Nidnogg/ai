3e60393fd21bc4a07de74c7dca2e8fe81e931a1f
nidnogg <henriquevt98@gmail.com>
2021-05-06 22:40:32 -0300

    finished random neighbors and neighbors

--- 6hillClimbing.py
+++ 6hillClimbing.py
@@ -1,10 +1,11 @@
-from random import randint
+import random
+import copy
 
 '''Returns a board of size N'''
 def board(N):
   board = []
   for x in range(0, N):
-    column_size = randint(0, N-1)
+    column_size = random.randint(0, N-1)
     board.append(column_size)
   print("original board is " + str(board))
   return board
@@ -15,25 +16,32 @@ being a neighbor to the current node
 ''' 
 def neighbors(board):
   neighbors = []
-  
+  editedBoardPositions = copy.deepcopy(board)
   for x in board:
-    i = board.index(x) 
+    i = editedBoardPositions.index(x)
     for y in range(0, len(board)):
-      currentNeighbor = board
-      if(x == y): 
+      currentNeighbor = copy.deepcopy(board)
+      if (x == y):
         continue
-      #print(currentNeighbor)
-      print(currentNeighbor)
       currentNeighbor[i] = y
+      editedBoardPositions[i] = -1      
       neighbors.append(currentNeighbor)
-    i = i + 1
-    
-  #print(neighbors)
+  print("printing neighbors")
+  for x in neighbors:
+    print(x)
+  return neighbors
 
-  
+
+def randomNeighbor(neighbors):
+  print("random neighbor")
+  print(random.choice(neighbors))
+  return random.choice(neighbors)
+
 
 def main():
   neighbors(board(4))
+  randomNeighbor(neighbors(board(4)))
+  
 
 if __name__ == '__main__':
     main()
