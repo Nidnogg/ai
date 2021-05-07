bdf34d22c2b5b2187b0280e6ce8d17f0cac15e92
nidnogg <henriquevt98@gmail.com>
2021-05-06 21:11:46 -0300

    glitchy neighbor 6hillclimbing

--- 6hillClimbing.py
+++ 6hillClimbing.py
@@ -1,23 +1,36 @@
-
 from random import randint
 
+'''Returns a board of size N'''
 def board(N):
   board = []
   for x in range(0, N):
     column_size = randint(0, N-1)
     board.append(column_size)
+  print("original board is " + str(board))
   return board
-    
+            
+'''
+Returns a list of sublists, each sublist 
+being a neighbor to the current node
+''' 
 def neighbors(board):
   neighbors = []
+  
   for x in board:
+    i = board.index(x) 
     for y in range(0, len(board)):
       currentNeighbor = board
-      if(y == x):
+      if(x == y): 
         continue
-      #print(currentNeighbor.index(x))
-      currentNeighbor[currentNeighbor.index(x)] = y 
-    neighbors.append(currentNeighbor)
+      #print(currentNeighbor)
+      print(currentNeighbor)
+      currentNeighbor[i] = y
+      neighbors.append(currentNeighbor)
+    i = i + 1
+    
+  #print(neighbors)
+
+  
 
 def main():
   neighbors(board(4))
